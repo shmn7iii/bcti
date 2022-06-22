8bbe9b174ae9121c77bfda8c0dab64cfec9fac84
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-06-22 10:26:40 +0900

    update: lint chap4-1

--- chap04_cryptographic-basics/python/chap4_1.py
+++ chap04_cryptographic-basics/python/chap4_1.py
@@ -4,25 +4,26 @@ import hashlib
 
 
 def sha40(x: str):
-    return hashlib.sha256(x.encode('utf-8')).hexdigest()[0:10]      # rubyとはスライスの仕様が違うので注意
+    # rubyとはスライスの仕様が違うので注意
+    return hashlib.sha256(x.encode('utf-8')).hexdigest()[0:10]
 
 
 def rho(h0):
     kame, usagi = h0, h0
     while True:
-        kame = sha40(kame)                  # single hashの系列
-        usagi = sha40(sha40(usagi))         # double hashの系列
-        if kame == usagi:                   # loop の合流
+        kame = sha40(kame)                # single hashの系列
+        usagi = sha40(sha40(usagi))       # double hashの系列
+        if kame == usagi:                 # loop の合流
             break
-    goryu = kame                            # 合流地点を記憶
+    goryu = kame                          # 合流地点を記憶
     kame = h0
     while True:
-        kame_p,goryu_p = kame,goryu           # 直前のデータ（ハッシュ値の原像）を記憶
-        kame = sha40(kame)                    # h0から再スタートする系列
-        goryu = sha40(goryu)                  # 合流地点からスタートする系列
-        if kame == goryu:                     # ハッシュ値が一致
+        kame_p, goryu_p = kame, goryu     # 直前のデータ（ハッシュ値の原像）を記憶
+        kame = sha40(kame)                # h0から再スタートする系列
+        goryu = sha40(goryu)              # 合流地点からスタートする系列
+        if kame == goryu:                 # ハッシュ値が一致
             break
-    return [kame_p, goryu_p]                  # ハッシュ値の原像ペアを出力
+    return [kame_p, goryu_p]              # ハッシュ値の原像ペアを出力
 
 
 pair = rho("0000000000")
